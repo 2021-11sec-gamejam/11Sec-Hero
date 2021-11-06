836c7553a9ecf8427d97f669c94a56f0ad2df52e
Kim sm <dev.sonohoshi@gmail.com>
2021-11-07 02:02:22 +0900

    Update GameManager.cs

--- Assets/_Scripts/Singleton/GameManager.cs
+++ Assets/_Scripts/Singleton/GameManager.cs
@@ -1,10 +1,18 @@
-﻿using Player;
+﻿using System;
+using Player;
 using UnityEngine;
 
 namespace Singleton
 {
     public class GameManager : Singleton<GameManager>
     {
+        [SerializeField] public GameObject playerSample;
+        
         public PlayerModel player;
+
+        protected void Start()
+        {
+            player ??= Instantiate(playerSample, Vector3.zero, Quaternion.identity).GetComponent<PlayerModel>();
+        }
     }
 }
