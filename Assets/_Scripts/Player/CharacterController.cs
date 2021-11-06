86517ad63f44c9ffd30e5038cd670241ff4df00d
Kim sm <dev.sonohoshi@gmail.com>
2021-11-07 01:40:21 +0900

    Update CharacterController.cs

--- Assets/_Scripts/Player/CharacterController.cs
+++ Assets/_Scripts/Player/CharacterController.cs
@@ -1,66 +1,66 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Player;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+namespace Player
 {
-    public PlayerModel model;
-    public float speed = 1f;
-
-    private void Update()
+    public class CharacterController : MonoBehaviour
     {
-        switch (model.state)
+        public PlayerModel model;
+        public float speed = 1f;
+
+        private void Update()
         {
-            case PlayerModel.PlayerState.Idle:
-                var input = GetMovingInput();
-                if (input != Vector2.zero)
-                {
-                    transform.Translate(input * (Time.deltaTime * speed));
-                    model.state = PlayerModel.PlayerState.Walk;
-                }
-                GetAttackInput();
-                break;
-            case PlayerModel.PlayerState.Walk:
-                input = GetMovingInput();
-                if (input != Vector2.zero)
-                {
-                    transform.Translate(input * (Time.deltaTime * speed));
-                }
-                else
-                {
-                    model.state = PlayerModel.PlayerState.Idle;
-                }
-                GetAttackInput();
-                break;
-            case PlayerModel.PlayerState.Attack:
-                if (!model.weapon.model.isAttacking)
-                {
-                    model.state = PlayerModel.PlayerState.Idle;
-                }
-                break;
-            case PlayerModel.PlayerState.Death:
-                break;
+            switch (model.state)
+            {
+                case PlayerModel.PlayerState.Idle:
+                    var input = GetMovingInput();
+                    if (input != Vector2.zero)
+                    {
+                        transform.Translate(input * (Time.deltaTime * speed));
+                        model.state = PlayerModel.PlayerState.Walk;
+                    }
+                    GetAttackInput();
+                    break;
+                case PlayerModel.PlayerState.Walk:
+                    input = GetMovingInput();
+                    if (input != Vector2.zero)
+                    {
+                        transform.Translate(input * (Time.deltaTime * speed));
+                    }
+                    else
+                    {
+                        model.state = PlayerModel.PlayerState.Idle;
+                    }
+                    GetAttackInput();
+                    break;
+                case PlayerModel.PlayerState.Attack:
+                    if (!model.weapon.model.isAttacking)
+                    {
+                        model.state = PlayerModel.PlayerState.Idle;
+                    }
+                    break;
+                case PlayerModel.PlayerState.Death:
+                    break;
+            }
         }
-    }
 
-    private static Vector2 GetMovingInput()
-    {
-        var x = 0f;
-        x += Input.GetKey(KeyCode.A) ? -1 : 0;
-        x += Input.GetKey(KeyCode.D) ? 1 : 0;
-        var y = 0f;
-        y += Input.GetKey(KeyCode.W) ? 1 : 0;
-        y += Input.GetKey(KeyCode.S) ? -1 : 0;
-        return new Vector2(x, y);
-    }
-
-    private void GetAttackInput()
-    {
-        if (Input.GetMouseButtonDown(1))
+        private static Vector2 GetMovingInput()
         {
-            model.weapon.Attack();
+            var x = 0f;
+            x += Input.GetKey(KeyCode.A) ? -1 : 0;
+            x += Input.GetKey(KeyCode.D) ? 1 : 0;
+            var y = 0f;
+            y += Input.GetKey(KeyCode.W) ? 1 : 0;
+            y += Input.GetKey(KeyCode.S) ? -1 : 0;
+            return new Vector2(x, y);
+        }
+
+        private void GetAttackInput()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                model.weapon.Attack();
+            }
         }
     }
 }
