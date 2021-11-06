227479f1a9b67d5223436420aa28451c050387ad
Kim sm <dev.sonohoshi@gmail.com>
2021-11-07 01:35:29 +0900

    Make CSV Parser and Weapon Factory

---
+++ Assets/_Scripts/Factory/WeaponFactory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Module;
+using Singleton;
+using UnityEngine;
+
+namespace Factory
+{
+    using Weapon;
+    
+    public class WeaponFactory : Singleton<WeaponFactory>
+    {
+        private List<Weapon.WeaponModel> _weapons;
+        private const string CsvPath = "Weapon";
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            var text = Resources.Load<TextAsset>(CsvPath);
+            _weapons = CsvParser.GetWeapons(text.text);
+        }
+
+        public Weapon.WeaponModel GetBasicWeapon()
+        {
+            return _weapons.First(model => model.weaponName.Equals("Entry_Sword"));
+        }
+    }
+}
---
+++ Assets/_Scripts/Module/CsvParser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module
+{
+    using Weapon;
+    public static class CsvParser
+    {
+        public static List<Weapon.WeaponModel> GetWeapons(string csv)
+        {
+            var strings = csv.Split('\n');
+            var list = new List<Weapon.WeaponModel>();
+            foreach (var line in strings.Skip(1))
+            {
+                var elements = line.Split(',');
+                var model = new Weapon.WeaponModel();
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (i == 1)
+                    {
+                        continue;
+                    }
+                    
+                    switch (i)
+                    {
+                        case 0:
+                            model.type = (Weapon.WeaponType) int.Parse(elements[i]);
+                            break;
+                        case 2:
+                            int.TryParse(elements[i], out model.rarity);
+                            break;
+                        case 3:
+                            model.weaponName = elements[i];
+                            break;
+                        case 4:
+                            float.TryParse(elements[i], out model.damageValue);
+                            break;
+                        case 5:
+                            float.TryParse(elements[i], out model.coolDown);
+                            break;
+                    }
+                }
+                list.Add(model);
+            }
+
+            return list;
+        }
+    }
+}
